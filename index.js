dc1126381be64001df1b3f9ed62be26378b9c541
Devon Khosa <dalvinderkhosa@gmail.com>
2021-02-15 15:15:19 -0700

    Created contact page

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+
+const nodemailer = require("nodemailer");
 const express = require('express');
 const app = express();
 
@@ -28,6 +31,34 @@ res.render('posts/2020-03-12', {});
 });
 app.get('/post-2', (req, res)=>{
 res.render('posts/2021-02-10', {});
+});
+
+//nodemailer//
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+app.get('/send',function(req,res){
+    var mailOptions={
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.render('emailFailed');
+     }else{
+            console.log("Message sent: " + response.message);
+        res.render('emailSent');
+         }
+});
 });
 
 //========End========//
